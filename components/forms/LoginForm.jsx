f521229732d4a644aaa7cb3a886c3e9b4f9b2be7
s-kvng <kvngnathan8420@gmail.com>
2024-04-24 09:00:03 +0000

    ui:update login form

--- components/forms/LoginForm.jsx
+++ components/forms/LoginForm.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { Input } from "@nextui-org/react";
+import { useForm } from "react-hook-form";
 
 import { MailIcon } from "../icons/MailIcon";
 import { EyeFilledIcon } from "../icons/EyeFilledIcon";
@@ -13,14 +14,28 @@ import { Divider } from "@nextui-org/react";
 import { GoogleIcon } from "../icons/GoogleIcon";
 
 const LoginForm = () => {
+  const { register, handleSubmit } = useForm();
   const [isVisible, setIsVisible] = useState(false);
+  const [value, setValue] = useState("");
 
+  // scheme for password validation
+  const validatePassword = (value) =>
+    value.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i);
+
+  // function to check password
+  const isInvalid = React.useMemo(() => {
+    if (value === "") return "default";
+
+    return validatePassword(value) ? "success" : "danger";
+  }, [value]);
+
+  // toggle show password
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   const onSubmit = (data) => console.log(data);
   return (
     <div className="flex items-center justify-center w-full z-10">
-      <div className={`mx-auto w-full max-w-lg bg-gray-200/50 rounded-xl p-10`}>
+      <div className={`mx-auto w-full max-w-lg bg-gray-200/60 rounded-xl p-10`}>
         <div className="mb-2 flex justify-center">
           <span className="inline-block w-full max-w-[60px]">
             <img src="/favicon.ico" alt="Logo" />
@@ -39,21 +54,27 @@ const LoginForm = () => {
           </Link>
         </p>
 
-        <form className=" z-20">
+        <form className=" z-20" onSubmit={handleSubmit(onSubmit)}>
           <div className=" space-y-8">
             <div>
               <Input
                 type="email"
-                variant="underlined"
+                variant="bordered"
                 label="Email"
                 isClearable
+                {...register("email", { required: true })}
               />
             </div>
             <div>
               <Input
+                value={value}
                 label="Password"
-                color="default"
+                // color="default"
                 variant="bordered"
+                onInvalid={isInvalid}
+                onValueChange={setValue}
+                color={isInvalid}
+                errorMessage={isInvalid && "Please enter a valid password"}
                 endContent={
                   <button
                     className="focus:outline-none"
@@ -69,6 +90,11 @@ const LoginForm = () => {
                 }
                 type={isVisible ? "text" : "password"}
                 className=""
+                {...register("password", {
+                  required: true,
+                  pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i,
+                  minLength: 8,
+                })}
               />
             </div>
           </div>
@@ -86,11 +112,11 @@ const LoginForm = () => {
           </div>
 
           <div className="text-center w-full">
-            <p className=" flex items-center mb-4">
+            <div className=" flex items-center mb-4">
               <Divider className=" w-[40%]" />
               <span className=" w-[20%]">OR</span>
               <Divider className=" w-[40%]" />
-            </p>
+            </div>
 
             <div className=" flex justify-center">
               <Link href={"/"}>
