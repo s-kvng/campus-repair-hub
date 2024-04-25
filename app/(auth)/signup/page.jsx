7458682fef97f367835526e544ed42f597d53172
s-kvng <kvngnathan8420@gmail.com>
2024-04-25 23:18:51 +0000

    ui: add signup form

---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,12 @@
+import SignUpForm from "@/components/forms/SignUpForm";
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignUp;
---
+++ components/forms/SignUpForm.jsx
@@ -0,0 +1,155 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { Input } from "@nextui-org/react";
+import { useForm } from "react-hook-form";
+
+import { MailIcon } from "../icons/MailIcon";
+import { EyeFilledIcon } from "../icons/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "../icons/EyeSlashFilledIcon";
+import { Button } from "../ui/button";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { Divider } from "@nextui-org/react";
+import { GoogleIcon } from "../icons/GoogleIcon";
+
+const SignUpForm = () => {
+  const { register, handleSubmit } = useForm();
+  const [isVisible, setIsVisible] = useState(false);
+  const [value, setValue] = useState("");
+
+  // scheme for password validation
+  const validatePassword = (value) =>
+    value.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i);
+
+  // function to check password
+  const isInvalid = React.useMemo(() => {
+    if (value === "") return "default";
+
+    return validatePassword(value) ? "success" : "danger";
+  }, [value]);
+
+  // toggle show password
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const onSubmit = (data) => console.log(data);
+  return (
+    <div className="flex items-center justify-center w-full z-10 bg-red-500 h-screen">
+      <div className={`mx-auto w-full max-w-lg bg-black rounded-xl p-10`}>
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[60px]">
+            <img src="/favicon.ico" alt="Logo" />
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight text-white">
+          Sign Up to your account
+        </h2>
+        <p className="mt-2 text-center text-base text-gray-600 mb-6">
+          Already have an account?&nbsp;
+          <Link
+            href="/login"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Login
+          </Link>
+        </p>
+
+        <form className=" z-20" onSubmit={handleSubmit(onSubmit)}>
+          <div className=" space-y-5">
+            <div>
+              <Input
+                type="text"
+                variant="bordered"
+                size="sm"
+                label="First Name"
+                isClearable
+                {...register("firstname", { required: true })}
+              />
+            </div>
+            <div>
+              <Input
+                type="text"
+                variant="bordered"
+                size="sm"
+                label="Last Name"
+                isClearable
+                {...register("lastname", { required: true })}
+              />
+            </div>
+            <div>
+              <Input
+                type="email"
+                variant="bordered"
+                size="sm"
+                label="Email"
+                isClearable
+                {...register("email", { required: true })}
+              />
+            </div>
+            <div>
+              <Input
+                value={value}
+                label="Password"
+                // color="default"
+                variant="bordered"
+                size="sm"
+                onInvalid={isInvalid}
+                onValueChange={setValue}
+                color={isInvalid}
+                errorMessage={isInvalid && "Please enter a valid password"}
+                endContent={
+                  <button
+                    className="focus:outline-none"
+                    type="button"
+                    onClick={toggleVisibility}
+                  >
+                    {isVisible ? (
+                      <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    ) : (
+                      <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                    )}
+                  </button>
+                }
+                type={isVisible ? "text" : "password"}
+                className=""
+                {...register("password", {
+                  required: true,
+                  pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i,
+                  minLength: 8,
+                })}
+              />
+            </div>
+          </div>
+
+          <div className="mt-5 mb-3">
+            <Button
+              variant="primary"
+              size="lg"
+              className="w-full"
+              // onClick={onSubmit}
+            >
+              <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+              Sign in
+            </Button>
+          </div>
+
+          <div className="text-center w-full">
+            <div className=" flex items-center mb-4">
+              <Divider className=" w-[40%]" />
+              <span className=" w-[20%]">OR</span>
+              <Divider className=" w-[40%]" />
+            </div>
+
+            <div className=" flex justify-center">
+              <Link href={"/"}>
+                <GoogleIcon />
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpForm;
