ded57d6376a90324f863c57a84aad7abe0bbfd6c
s-kvng <kvngnathan8420@gmail.com>
2024-04-30 09:06:19 +0000

    feat: complete user signup

--- components/forms/SignUpForm.jsx
+++ components/forms/SignUpForm.jsx
@@ -6,7 +6,6 @@ import { useRouter } from "next/navigation";
 import { Input } from "@nextui-org/react";
 import { useForm } from "react-hook-form";
 
-import { MailIcon } from "../icons/MailIcon";
 import { EyeFilledIcon } from "../icons/EyeFilledIcon";
 import { EyeSlashFilledIcon } from "../icons/EyeSlashFilledIcon";
 import { Button } from "../ui/button";
@@ -15,6 +14,8 @@ import { Divider } from "@nextui-org/react";
 import { GoogleIcon } from "../icons/GoogleIcon";
 import useAuth from "@/context/useAuth";
 import appwriteService from "@/appwrite/config";
+import { message } from "antd";
+import { CircularProgress } from "@nextui-org/react";
 
 const SignUpForm = () => {
   const router = useRouter();
@@ -26,6 +27,8 @@ const SignUpForm = () => {
   } = useForm();
   const [isVisible, setIsVisible] = useState(false);
   const [value, setValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   // scheme for password validation
   const validatePassword = (value) =>
@@ -42,22 +45,27 @@ const SignUpForm = () => {
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   const onSubmit = async (data) => {
+    setIsLoading(true);
     const { firstname, lastname, email, password } = data;
+    const name = `${firstname} ${lastname}`;
 
-    console.log(
-      `First Name: ${firstname}, Last Name: ${lastname}, Email: ${email} and Password: ${password}`
-    );
-    //   try {
-    //     const userData = await appwriteService.createUserAccount({email, password, name})
-    //     if(userData){
-    //         setAuthStatus(true)
-    //         setIsLoading(false)
-    //         router.push("/profile")
-    //     }
-    // } catch (error) {
-    //     setError(error.message)
-    //     setIsLoading(false)
-    // }
+    try {
+      const userData = await appwriteService.createUserAccount({
+        email,
+        password,
+        name,
+      });
+      if (userData) {
+        setAuthStatus(true);
+        setIsLoading(false);
+        message.success(`Your account has been created, Mr. ${firstname}`);
+        router.push("/profile");
+      }
+    } catch (error) {
+      setError(error.message);
+      message.error("Something went wrong");
+      setIsLoading(false);
+    }
   };
   return (
     <div className="flex items-center justify-center w-full z-10 py-8 px-4 sm:px-0">
@@ -91,112 +99,127 @@ const SignUpForm = () => {
           </Link>
         </p>
 
-        <form className=" z-20" onSubmit={handleSubmit(onSubmit)}>
-          <div className=" space-y-5">
-            <div>
-              <Input
-                type="text"
-                variant="bordered"
-                size="sm"
-                label="First Name"
-                isClearable
-                {...register("firstname", { required: true })}
-              />
-              {errors.firstname && (
-                <span className=" text-red-500">
-                  First name field is required
-                </span>
-              )}
-            </div>
-            <div>
-              <Input
-                type="text"
-                variant="bordered"
-                size="sm"
-                label="Last Name"
-                isClearable
-                {...register("lastname", { required: true })}
-              />
-              {errors.lastname && (
-                <span className=" text-red-500">
-                  Last name field is required
-                </span>
-              )}
-            </div>
-            <div>
-              <Input
-                type="email"
-                variant="bordered"
-                size="sm"
-                label="Email"
-                isClearable
-                {...register("email", { required: true })}
-              />
-              {errors.email && (
-                <span className=" text-red-500">Email field is required</span>
-              )}
-            </div>
-            <div>
-              <Input
-                value={value}
-                label="Password"
-                // color="default"
-                variant="bordered"
-                size="sm"
-                onInvalid={isInvalid}
-                onValueChange={setValue}
-                color={isInvalid}
-                errorMessage={isInvalid && "Please enter a valid password"}
-                endContent={
-                  <button
-                    className="focus:outline-none"
-                    type="button"
-                    onClick={toggleVisibility}
-                  >
-                    {isVisible ? (
-                      <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                    ) : (
-                      <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
-                    )}
-                  </button>
-                }
-                type={isVisible ? "text" : "password"}
-                className=""
-                {...register("password", {
-                  required: true,
-                  pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i,
-                  minLength: 8,
-                })}
+        {isLoading ? (
+          <div className=" z-20 w-full">
+            <div className="flex items-center justify-center">
+              <CircularProgress
+                size="lg"
+                color="primary"
+                aria-label="loading..."
               />
             </div>
           </div>
-
-          <div className="mt-5 mb-3">
-            <Button
-              variant="primary"
-              size="lg"
-              className="w-full"
-              // onClick={onSubmit}
-            >
-              <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-              Sign Up
-            </Button>
-          </div>
-
-          <div className="text-center w-full">
-            <div className=" flex items-center mb-4">
-              <Divider className=" w-[40%]" />
-              <span className=" w-[20%]">OR</span>
-              <Divider className=" w-[40%]" />
+        ) : (
+          <form className=" z-20" onSubmit={handleSubmit(onSubmit)}>
+            <div className=" space-y-5">
+              <div>
+                <Input
+                  type="text"
+                  variant="bordered"
+                  size="sm"
+                  label="First Name"
+                  isClearable
+                  {...register("firstname", { required: true })}
+                />
+                {errors.firstname && (
+                  <span className=" text-red-500">
+                    First name field is required
+                  </span>
+                )}
+              </div>
+              <div>
+                <Input
+                  type="text"
+                  variant="bordered"
+                  size="sm"
+                  label="Last Name"
+                  isClearable
+                  {...register("lastname", { required: true })}
+                />
+                {errors.lastname && (
+                  <span className=" text-red-500">
+                    Last name field is required
+                  </span>
+                )}
+              </div>
+              <div>
+                <Input
+                  type="email"
+                  variant="bordered"
+                  size="sm"
+                  label="Email"
+                  isClearable
+                  {...register("email", { required: true })}
+                />
+                {errors.email && (
+                  <span className=" text-red-500">Email field is required</span>
+                )}
+              </div>
+              <div>
+                <Input
+                  value={value}
+                  label="Password"
+                  // color="default"
+                  variant="bordered"
+                  size="sm"
+                  onInvalid={isInvalid}
+                  onValueChange={setValue}
+                  color={isInvalid}
+                  errorMessage={isInvalid && "Please enter a valid password"}
+                  endContent={
+                    <button
+                      className="focus:outline-none"
+                      type="button"
+                      onClick={toggleVisibility}
+                    >
+                      {isVisible ? (
+                        <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                      ) : (
+                        <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                      )}
+                    </button>
+                  }
+                  type={isVisible ? "text" : "password"}
+                  className=""
+                  {...register("password", {
+                    required: true,
+                    pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i,
+                    minLength: 8,
+                  })}
+                />
+              </div>
             </div>
 
-            <div className=" flex justify-center">
-              <Link href={"/"}>
-                <GoogleIcon />
-              </Link>
+            <div className="mt-5 mb-3">
+              <Button
+                variant="primary"
+                size="lg"
+                className="w-full"
+                disabled={isLoading}
+                // onClick={onSubmit}
+              >
+                {isLoading && (
+                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                )}
+                Sign Up
+              </Button>
             </div>
-          </div>
-        </form>
+
+            <div className="text-center w-full">
+              <div className=" flex items-center mb-4">
+                <Divider className=" w-[40%]" />
+                <span className=" w-[20%]">OR</span>
+                <Divider className=" w-[40%]" />
+              </div>
+
+              <div className=" flex justify-center">
+                <Link href={"/"}>
+                  <GoogleIcon />
+                </Link>
+              </div>
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
