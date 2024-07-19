eb658ecf833c391bbac1ad5aa7a80dbfdfdc7e5d
s-kvng <kvngnathan8420@gmail.com>
2024-07-19 06:22:58 +0000

    ui: user profile page

--- app/(protected)/(user)/profile/page.jsx
+++ app/(protected)/(user)/profile/page.jsx
@@ -2,11 +2,35 @@
 
 import { useUserContext } from "@/context/AuthContext";
 import React from "react";
+import { Tabs, Space } from "antd";
 
 const Profile = () => {
+  const tabItems = [
+    {
+      label: `Info`,
+      key: `1`,
+      children: `Content of Tab 1`,
+    },
+    {
+      label: `Pending Requests`,
+      key: `2`,
+      children: `Content of Tab 2`,
+    },
+  ];
+
   const { user } = useUserContext();
   console.log(user.repairer);
-  return <div className="text-black">Profile {user.firstname}</div>;
+  return (
+    <div className="text-black min-h-[75vh]">
+      <div>Profile {user.firstname}</div>
+      <Tabs
+        tabPosition={"left"}
+        items={tabItems.map((tabItem) => {
+          return tabItem;
+        })}
+      />
+    </div>
+  );
 };
 
 export default Profile;
